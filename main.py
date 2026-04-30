add0e846dbac211746a3414c9bfaf6338ce9a2d4
Patricia Tran <patrici9tran@gmail.com>
2026-04-29 22:19:52 -0400

    beginning screen

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import numpy as np
+from sense_hat import SenseHat
+
+#beginning screen
+
+def beginning_screen():
+    sense.set_rotation(180)
+    sense.show_message("CONNECT", text_colour=(0, 0, 255))
+    sense.show_message("4", text_colour=(255, 0, 0), scroll_speed=0.05)
+    sense.show_message("PRESS2PLAY", text_colour=(255, 0, 0))
+
+#creates matrix full of zeros
+def create_board():
+    board = numpy.zeros((6,7))
+    return board
+
+sense = SenseHat()
+
+while True:
+    beginning_screen();
+sense.clear()
+
